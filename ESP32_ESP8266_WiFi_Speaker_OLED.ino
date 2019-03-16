c1e87fdfa6d83e70fd7e72e50c9ff85b101e6b53
smurf0969 <47907842+smurf0969@users.noreply.github.com>
2019-03-16 11:03:24 +0000

    Initial commit

---
+++ ESP32_ESP8266_WiFi_Speaker_OLED.ino
@@ -0,0 +1,362 @@
+#include <FS.h> //this needs to be first, or it all crashes and burns...
+#include "WiFiConnectOLED.h" //include before SSD1306.h if using custom fonts
+
+#include "play.h"
+#include "error.h"
+#include "logo.h"
+
+#ifdef ESP32
+#include "Arduino.h"
+#include "pins_arduino.h" // Heltec_WIFI_Kit_32 pins
+
+#include <SPIFFS.h> // Used to save custom parameters
+#endif
+
+#include <ArduinoJson.h> //https://github.com/bblanchon/ArduinoJson
+
+//******* OLED *********
+
+#include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
+/*
+   Origional library https://github.com/ThingPulse/esp8266-oled-ssd1306
+   Customised library https://github.com/smurf0969/esp8266-oled-ssd1306/tree/Allow-overriding-default-font
+*/
+#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"
+
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// connect ESP12E  D1 to SCL, D2 to SDA
+//OLED pins to ESP32 GPIOs via this connecthin:
+//OLED_SDA -- GPIO4
+//OLED_SCL -- GPIO15
+//OLED_RST -- GPIO16
+
+#ifdef ESP32
+#define OLED_RESET 16
+#else
+#define OLED_RESET 10
+#endif
+
+#ifdef ESP32
+SSD1306  display(0x3c, 4, 15);
+#define oledPWR 16 //Pin to supply power to OLED
+#else
+SSD1306  display(0x3c, 4, 5);
+#define oledPWR 10 //Pin to supply power to OLED
+#endif
+
+/* Audio */
+#include "AudioFileSourceHTTPStream.h"
+#include "AudioFileSourceBuffer.h"
+#include "AudioGeneratorMP3.h"
+#include "AudioOutputI2S.h"
+
+#if !defined(INTERNAL_DAC)
+#define INTERNAL_DAC 1
+#endif
+// To run, set your ESP8266 build to 160MHz,  and upload.
+AudioGeneratorMP3 *mp3;
+AudioFileSourceHTTPStream  *file;
+AudioFileSourceBuffer *buff;
+AudioOutputI2S *out;
+
+int lastCode = 0;
+int buffUnderflow = 0;
+// Called when there's a warning or error (like a buffer underflow or decode hiccup)
+void StatusCallback(void *cbData, int code, const char *string)
+{
+  if (code == 3 && lastCode != 2) {
+    buffUnderflow += 1;
+  }
+  lastCode = code;
+  const char *ptr = reinterpret_cast<const char *>(cbData);
+  // Note that the string may be in PROGMEM, so copy it to RAM for printf
+  char s1[64];
+  strncpy_P(s1, string, sizeof(s1));
+  s1[sizeof(s1) - 1] = 0;
+  Serial.printf("STATUS(%s) '%d' = '%s'\n", ptr, code, s1);
+  Serial.flush();
+}
+
+
+/* Our custom Parameters */
+char URL[261] = "http://192.168.1.187:8000/live"; //TODO
+char server_ip[128] = "192.168.1.187";
+char server_port[5] = "8000";
+char server_path[128] = "/live";
+void setMp3URL() {
+  String s = "http://" + String(server_ip) + (strlen(server_port) > 0 ? ":" + String(server_port) : "") + (strlen(server_path) > 0 ? String(server_path) : "");
+  strcpy(URL, s.c_str());
+  Serial.println(URL);
+}
+int json_doc_size = 256;
+WiFiConnectParam custom_server_ip("server_ip", "server_ip", server_ip, 128);
+WiFiConnectParam custom_server_port("server_port", "server_port", server_port, 5);
+WiFiConnectParam custom_server_path("server_path", "server_path", server_path, 128);
+
+WiFiConnectOLED wc(&display, oledPWR); //Initialise our connector with an OLED display
+
+/*
+    Callback for when parameters need saving.
+    Can be used to set a flag preferably with saving taking place in main loop.
+
+*/
+bool configNeedsSaving = false;
+void saveConfigCallback() {
+  Serial.println("Should save config");
+  configNeedsSaving = true;
+}
+void configModeCallback(WiFiConnect *mWiFiConnect) {
+  Serial.println("Entering Access Point");
+}
+/* Save our custom parameters */
+void saveConfiguration() {
+  configNeedsSaving = false;
+  if (!SPIFFS.begin()) {
+    Serial.println("UNABLE to open SPIFFS");
+    return;
+  }
+
+  strcpy(server_ip, custom_server_ip.getValue());
+  strcpy(server_port, custom_server_port.getValue());
+  strcpy(server_path, custom_server_path.getValue());
+
+  Serial.println("saving config");
+  DynamicJsonDocument doc(json_doc_size);
+
+
+  doc["server_ip"] = server_ip;
+  doc["server_port"] = server_port;
+  doc["server_path"] = server_path;
+
+  File configFile = SPIFFS.open("/config.json", "w");
+
+  if (!configFile) {
+    Serial.println("failed to open config file for writing");
+  }
+  serializeJson(doc, Serial);
+  Serial.println("");
+  serializeJson(doc, configFile);
+  configFile.close();
+  //end save
+  SPIFFS.end();
+  setMp3URL();
+  Serial.println(URL);
+}
+
+
+void startWiFi(boolean showParams = false) {
+  wc.begin(true);
+  //wc.setDebug(true);
+  /* Set our callbacks */
+  wc.setSaveConfigCallback(saveConfigCallback);
+  wc.setAPCallback(configModeCallback);
+  /* Add some custom parameters */
+  wc.addParameter(&custom_server_ip);
+  wc.addParameter(&custom_server_port);
+  wc.addParameter(&custom_server_path);
+
+  //wc.screenTest(); //test screen by cycling through the presete screens
+  //wc.resetSettings(); //helper to remove the stored wifi connection, comment out after first upload and re upload
+
+  if (!showParams) {
+    /*
+       AP_NONE = Continue executing code
+       AP_LOOP = Trap in a continuous loop
+       AP_RESET = Restart the chip
+    */
+    if (!wc.autoConnect()) { // try to connect to wifi
+      /* We could also use button etc. to trigger the portal on demand within main loop */
+      wc.startConfigurationPortal(AP_LOOP);//if not connected show the configuration portal
+    }
+  } else {
+    /* We could also use button etc. to trigger the portal on demand within main loop */
+    wc.startParamsPortal(AP_NONE); //Show the parameters portal
+  }
+  // when displayLoop is called from main loop, will turn of display after time period
+  wc.displayTurnOFF((60 * 1000 * 10));
+}
+
+void loadConfiguration() {
+  if (!SPIFFS.begin()) {
+    Serial.println(F("Unable to start SPIFFS"));
+    while (1) {
+      delay(1000);
+    }
+  } else {
+    Serial.println(F("mounted file system"));
+
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println(F("opened config file"));
+        size_t sizec = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[sizec]);
+
+        configFile.readBytes(buf.get(), sizec);
+        DynamicJsonDocument doc(1024);
+        DeserializationError error = deserializeJson(doc, buf.get());
+        if (error) {
+          Serial.println("failed to load json config");
+        } else {
+          strcpy(server_ip, doc["server_ip"]);
+          strcpy(server_port, doc["server_port"]);
+          strcpy(server_path, doc["server_path"]);
+          custom_server_ip.setValue(server_ip);
+          custom_server_port.setValue(server_port);
+          custom_server_path.setValue(server_path);
+        }
+        configFile.close();
+
+      }
+    } else {
+      Serial.println(F("Config file not found"));
+    }
+    SPIFFS.end();
+  }
+  setMp3URL();
+}
+
+const int preallocateBufferSize = 5 * 1024;
+//const int preallocateBufferSize = bufferMultiply * 1024;
+const int preallocateCodecSize = 29192; // MP3 codec max mem needed
+void *preallocateBuffer = NULL;
+void *preallocateCodec = NULL;
+
+bool useBuff = false;
+bool buffReserved = false;
+bool codecReserved = false;
+
+void startAudio(bool del = false) {
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.drawStringMaxWidth(128 / 2, (64 / 2) - 20, 128 - 10, F("Trying to start audio"));
+  display.display();
+  delay(500);
+  if (del) {
+    delete mp3;
+    //delete out;
+    delete buff;
+    delete file;
+    delay(100);
+    if (useBuff && buffUnderflow >= 5) {
+      useBuff = false;
+      free(preallocateBuffer);
+      buffReserved = false;
+    }
+  }
+  if (useBuff && !buffReserved) {
+    buffReserved = true;
+    preallocateBuffer = malloc(preallocateBufferSize);
+  }
+  if (!codecReserved) {
+    codecReserved = true;
+    preallocateCodec = malloc(preallocateCodecSize);
+  }
+  if ((useBuff && !preallocateBuffer) || !preallocateCodec) {
+    Serial.printf_P(PSTR("FATAL ERROR:  Unable to preallocate %d bytes for app\n"), preallocateBufferSize + preallocateCodecSize);
+    Serial.printf("FreeHeap: %d\n", ESP.getFreeHeap());
+    Serial.printf("Buffer: %s, %d\n", (!preallocateBuffer ? "false" : "true"), (!preallocateBuffer ? 0 : preallocateCodecSize));
+    Serial.printf("Codec: %s, %d\n", (!preallocateCodec ? "false" : "true"), (!preallocateCodec ? 0 : preallocateCodecSize));
+
+    while (1) delay(1000); // Infinite halt
+  } else {
+    Serial.println("");
+    if (useBuff) {
+      Serial.printf_P(PSTR("Preallocate %d bytes for Buffer\n"), preallocateBufferSize);
+    }
+    Serial.printf_P(PSTR("Preallocate %d bytes for Codec\n"), preallocateCodecSize);
+  }
+
+  Serial.println();
+  Serial.print(F("Buffer Underflow Count: "));
+  Serial.println(buffUnderflow);
+  file = new AudioFileSourceHTTPStream(URL);
+  if (useBuff) {
+    buff = new AudioFileSourceBuffer(file, 2048);
+    buff->RegisterStatusCB(StatusCallback, (void*)"buffer");
+  }
+#ifdef ESP32
+  out = new AudioOutputI2S(0, INTERNAL_DAC);
+#else
+  out = new AudioOutputI2S();
+  out->SetGain(0.2); // 0 - 4, 0.3 lower values reduce crackle
+  out->SetRate(44100);
+  out->SetBitsPerSample(16);
+  out->SetChannels(2);
+#endif
+  mp3 =  new AudioGeneratorMP3(preallocateCodec, preallocateCodecSize);;
+  //mp3->RegisterStatusCB(StatusCallback, (void*)"mp3");
+  if (useBuff) {
+    Serial.println(F("Using Audio Buffer"));
+    mp3->begin(buff, out);
+  } else {
+    Serial.println(F("No Audio Buffer"));
+    mp3->begin(file, out);
+  }
+  Serial.printf("FreeHeap: %d\n", ESP.getFreeHeap()); //Too low on free heap and we will never work!!
+}
+
+void setup() {
+#ifdef ESP8266
+  system_update_cpu_freq(SYS_CPU_160MHZ);
+#endif
+delay(100);
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(100);
+  }
+
+  loadConfiguration();
+
+  startWiFi();
+  delay(1500);
+  display.clear();
+  display.drawXbm(0, 0, logoDataW, logoDataH, logoData);
+  display.display();
+  delay(1500);
+  startAudio();
+}
+
+void loop() {
+  static int lastms = 0;
+  if (configNeedsSaving) {
+    saveConfiguration();
+  }
+
+  //  if (WiFi.status() != WL_CONNECTED) {
+  //    if (!wc.autoConnect()) wc.startConfigurationPortal(AP_RESET);
+  //  }
+  if (mp3->isRunning()) {
+    if (lastms == 0) {
+      display.clear();
+      display.drawXbm(0, 0, playDataW, playDataH, playData);
+      display.display();
+    }
+    if (millis() - lastms > 1000) {
+      lastms = millis();
+      //Serial.printf("Running for %d ms...\n", lastms);
+      //Serial.flush();
+    }
+    if (!mp3->loop()) {
+      mp3->stop();
+      lastms = 0;
+    }
+  } else {
+
+    display.clear();
+    display.drawXbm(0, 0, errorDataW, errorDataH, errorData);
+    display.display();
+    lastms = 0;
+    Serial.printf("MP3 done\n");
+    delay(3000);
+    if (!wc.autoConnect()) {
+      wc.startConfigurationPortal(AP_RESET);
+    }
+    startAudio(true);
+  }
+}
---
+++ README.md
@@ -0,0 +1,54 @@
+# ESP32 & ESP8266 WiFi Speaker with OLED display
+A MP3 streaming WiFi speaker for ESP8266 & ESP32 chips
+
+## Overview
+During a recent pantomime production we were unable to get the internal building sound system working and had to come up with a cheap solution to pipe music into the dressing rooms.
+Cobbling together a Raspberry Pi with sound card, ffmpeg and node rmtp server we were able to stream live AAC audio to a couple of mobiles running VLC Player. However due to the limitations of running live streaming on a PI, poor WiFi in the building and the buffering lag of VLC we ended up with a stuttering live stream that could be upto 40 seconds behind depending on the numer of connections, which was difficult for the cast and callers.
+
+Having a couple of node mcu ESP8266 12E development boards that were destined for some extra home automation, I decided to see if it was possible to make a WiFi speaker that would perform better than our previous solution.
+That was the basis for my first attempt [ESP8266_WiFi_Speaker](https://github.com/smurf0969/ESP8266_WiFi_Speaker) and after puchasing a Heltec WiFi Kit ESP32 with an onboard OLED display and having problems with wifi managers I decided to create a new wifi manager library [WiFiConnect](https://github.com/smurf0969/WiFiConnect) and convert my previous project to support both chips.
+
+## Components  
+   ### ESP8266
+       * ESP8266 ESP-12E Development Board NodeMcu
+       * 2x 4ohm 3W Loudspeaker
+       * Mini 3W+3W DC 5V Audio Amplifier PAM8043
+       * I2S PCM5102 DAC Decoder ( [ESP8266Audio by Earle F. Philhower, III](https://github.com/earlephilhower/ESP8266Audio) does allow for no decoder, but I have not tried it. )
+       * 0.96" I2C IIC Serial 128X64 White OLED LCD LED 
+
+       First major component problem was figuring out the DAC connections to use with AudioOutputI2S in the [ESP8266Audio](https://github.com/earlephilhower/ESP8266Audio) library.  
+       The configuration I finnally manages to get to work was:  
+          > 3.3V from ESP8266 -> VCC, 33V, XMT  
+          > GND from ESP8266 -> GND, FLT, DMP, FMT, SCL  
+          > GPIO15 (D8,TXD2) from ESP8266 -> BCK  
+          > GPIO3 (RX,RXD0) from ESP8266 -> DIN  
+          > GPIO2 (D4, TXD1) from ESP8266 -> LCK  
+   
+        The 5V Audio Amplifier PAM8043 needs a lot of juice and I found that as mine has a potentiometer I needed to turn it fully down to be able to program or run the ESP when only connected by USB serial connection. Powering from a USB power adapter for normal use, I had no problems.
+
+   ### ESP32  
+        * Heltec WiFi Kit ESP32 with an onboard 128x64 OLED display
+        * 2x 4ohm 3W Loudspeaker
+        * Mini 3W+3W DC 5V Audio Amplifier PAM8043
+
+        As the ESP32 has 2 onboard DAC's I decided to use them rather than use another decoder.
+
+## IDE settings & Library Versions
+Please check/review instructions and screenshots in the [Wiki](https://github.com/smurf0969/ESP32_ESP8266_WiFi_Speaker_OLED/wiki)
+
+## Known Issues
+ * Using buffering for the stream can cause alot of buffer underruns causing alot of stutter.
+ * With newer libraries installed compared to the previous project I have been unable to get a stable connection using the ESP12E unless I set the IwIP Variant to IPv6 Higher Bandwidth.
+ * Powering off of a com port can cause issues unless the amplifier is turned right down or off for uploading and playing music at higher volumes.
+
+## Putting it all together
+Due to the possibility of frequent IP changes of my Raspberry Pi server I have used [WiFiConnect](https://github.com/smurf0969/WiFiConnect) and its custom paramters.
+After trying lots of different configurations for the server mp3 encoding(ffmpeg,darkice) and distribution(icecast2,shoutcast), I have settled on using [Icecast2](http://icecast.org/) and [DarkIce](http://www.darkice.org/) with the following [Darkice configuration](./docs/darkice.cfg).
+
+## Thanks
+Many thanks to the authors and contibutors for the main libraries that made this project possible  
+  - ArduinoJson 6.9.1 [https://github.com/bblanchon/ArduinoJson.git](https://github.com/bblanchon/ArduinoJson.git) or [https://blog.benoitblanchon.fr](https://blog.benoitblanchon.fr)
+  - ESP8266Audio 1.1.3 [https://github.com/earlephilhower/ESP8266Audio](https://github.com/earlephilhower/ESP8266Audio)
+  - ESP8266 and ESP32 Oled Driver for SSD1306 display 4.0.0 [https://github.com/ThingPulse/esp8266-oled-ssd1306](https://github.com/ThingPulse/esp8266-oled-ssd1306) (I use a modified version,see [WiFiConnect]())
+
+Stuart Blair (smurf0969)
